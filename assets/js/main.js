527feb9fb7f277136125ae2b8ad300a02627b1ee
Kauã <kauataylor@gmail.com>
2024-03-05 20:50:47 -0300

    chore: add show nav bar logic

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,14 @@ const navToggleBtn = document.getElementById("navToggle"),
   navMenu = document.getElementById("navMenu"),
   navCloseBtn = document.getElementById("navClose");
 
-navToggleBtn.addEventListener("click", () => {
-  navMenu.classList.add("nav__menu--show");
-});
+if (navToggleBtn) {
+  navToggleBtn.addEventListener("click", () => {
+    navMenu.classList.add("nav__menu--show");
+  });
+}
 
-navCloseBtn.addEventListener("click", () => {
-  navMenu.classList.remove("nav__menu--show");
-});
+if (navCloseBtn) {
+  navCloseBtn.addEventListener("click", () => {
+    navMenu.classList.remove("nav__menu--show");
+  });
+}
